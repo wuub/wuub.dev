2a786ebe4a6b58be6262071296f85bf3da01e8b4
Wojciech Bederski <github@wuub.net>
2019-05-19 12:26:44 +0200

    initial setup

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,21 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Wojciech Bederski'
-SITENAME = 'wuub.dev blog'
-SITEURL = ''
+AUTHOR = "wuub"
+SITENAME = "wuub.dev blog"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/Warsaw'
 
-DEFAULT_LANG = 'en'
+THEME = "themes/brutalist"
+TWITTER_USERNAME = "@wuub"
+FIRST_NAME = "wuub"
+MENUITEMS = [("tags", "/tags")]
+LOGO = "logo.png"
+
+TIMEZONE = "Europe/Warsaw"
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,16 +26,24 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+SOCIAL = ()
 DEFAULT_PAGINATION = 10
 
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = [
+    "assets",
+    "sitemap",
+    "gravatar",
+    "liquid_tags.img",
+    "liquid_tags.video",
+    "liquid_tags.youtube",
+    "liquid_tags.include_code",
+]
+
+SITEMAP = {"format": "txt"}
+
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
